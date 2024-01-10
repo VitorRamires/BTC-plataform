a5a551bc7faa87ede574b7ab82c9e6523be6c607
VitorRamires <vmr.rs@hotmail.com>
2024-01-10 09:17:53 -0300

    ajustando código

--- JS/mostrarMoeda.js
+++ JS/mostrarMoeda.js
@@ -1,48 +1,64 @@
+  const anterior = document.querySelector('.anterior')
+  const proximo = document.querySelector('.proximo')
   const moedaTitle = Array.from(document.querySelectorAll('.title'))
   const moedaValue = Array.from(document.querySelectorAll('.value'))
   const moeda = Array.from(document.querySelectorAll('.moeda'))
-  const proximo = document.querySelector('.proximo')
-  const anterior = document.querySelector('.anterior')
   const barraProgresso = document.querySelector('.progresso')
+  const painelMoedaBox = document.querySelector('.painel-moedas-box')
   let counterSlide = 0
   let windowMatchMedia = window.matchMedia("(max-width:680px)")
+  
 
 
   function fetchValueMostrarMoeda(){
-   fetch('https://blockchain.info/ticker')
-   .then(response=>{
+    fetch('https://blockchain.info/ticker')
+    .then(response=>{
       let responseJson = response.json()
       return responseJson
-     })
+    })
     .then(responseJson => {
       let allCurrencys = responseJson
       return allCurrencys
     })
     .then((allCurrencys) => {
       let arrayCurrencys = Object.entries(allCurrencys) 
-      moedaTitle.forEach((item, index)=>{
-        item.innerHTML = arrayCurrencys[index][0]
-      })
-      moedaValue.forEach((item, index)=>{
-        item.innerHTML = arrayCurrencys[index][1].sell
-      })
+      estilizando(arrayCurrencys)
     })
   }
 
- 
-  function estilizando(){
-    moeda.forEach((item, index)=>{
-      item.style.left = index * 33 + '%'
-      if(windowMatchMedia.matches){
-        item.style.left = index * 100 + '%'
-      } else {
-        item.style.left = index * 33 + '%'
+  
+  function estilizando(arrayCurrencys){
+    arrayCurrencys.forEach((item, index)=>{
+      if(verificador === false){
+        let createMoeda = document.createElement('div')
+        let createTitulo = document.createElement('p')
+        let createValor = document.createElement('p')
+
+        createMoeda.classList.add('moeda')
+        createTitulo.classList.add('title')
+        createValor.classList.add('value')
+
+        painelMoedaBox.appendChild(createMoeda)
+        createMoeda.appendChild(createTitulo)
+        createMoeda.appendChild(createValor)
+
+        createTitulo.innerHTML = arrayCurrencys[index][0]
+        createValor.innerHTML = arrayCurrencys[index][1].sell
+
+        createMoeda.style.left = index * 33 + '%'
+        windowMatchMedia.matches ? createMoeda.style.left = index * 100 + '%' : createMoeda.style.left = index * 33 + '%'
+
+        return
       }
     })
+
+    verificador = true
   }
+  
 
   function slideHandle(){
-    moeda.forEach(item=>{
+    let moedas = document.querySelectorAll('.moeda')
+    moedas.forEach(item=>{
       item.style.transform = `translateX(-${counterSlide * 100}%)`
       if(windowMatchMedia.matches){
         counterSlide > 27 ? counterSlide = 27 : barraProgresso.style.width = counterSlide * 3.7 + '%'
@@ -64,7 +80,7 @@
     console.log(counterSlide)
   }
 
-  estilizando(), fetchValueMostrarMoeda()
+  fetchValueMostrarMoeda()
   setInterval(fetchValueMostrarMoeda, 5000)
   proximo.addEventListener('click',()=>{next()})
   anterior.addEventListener('click', ()=>{previous()})

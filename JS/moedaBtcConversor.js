c736e00d0ea6de9deee8527f67bb031e7f6a7d48
Vitor Ramires <vmr.rs@hotmail.com>
2025-01-04 23:05:42 -0300

    fix: scroll not working

--- JS/moedaBtcConversor.js
+++ JS/moedaBtcConversor.js
@@ -1,65 +1,72 @@
-import FetchHandle from './fetch.js'; 
-import handleElements from './script.js';
+import FetchHandle from "./fetch.js";
+import handleElements from "./script.js";
 
-export default function conversorMoedaParaBtc(){
-  const painelConvesor = document.querySelector('.conversor-painel')
-  const BtnConversor = document.querySelector('.botao-converter')
-  const conversorInput = document.querySelector('#input-btc')
-  let verificadorConversor = false
+export default function conversorMoedaParaBtc() {
+  const painelConvesor = document.querySelector(".conversor-painel");
+  const BtnConversor = document.querySelector(".botao-converter");
+  const conversorInput = document.querySelector("#input-btc");
+  let verificadorConversor = false;
 
-  function fetchBTCconversorMoeda(){
-    let fetching = FetchHandle.fetchUrl('https://blockchain.info/ticker');
+  function fetchBTCconversorMoeda() {
+    let fetching = FetchHandle.fetchUrl("https://blockchain.info/ticker");
     fetching.then((arrayCurrencys) => {
-      converterHandler(arrayCurrencys) 
-    })
-  }
+      converterHandler(arrayCurrencys);
+    });
 
-  function converterHandler(BTC){
-    let BTCvalue = conversorInput.value
-    if(BTCvalue === '' || BTCvalue === '0'){
-      alert('Preencha corretamente')
-      conversorInput.value = ''
-      return
-    } 
-
-    BTC.forEach((item, index)=>{
-      let calculo = Number(BTCvalue * BTC[index][1].sell).toFixed(0)
-      if((verificadorConversor === false)){
-
-        let createElement = document.createElement('div')
-        let handleReturn = handleElements('title-conversor', 'value-conversor', createElement)
-        
-        createElement.classList.add('moedaBtc-conversor', 'showOn', 'convertido')
-        painelConvesor.classList.add('growHeigth')
-
-        painelConvesor.appendChild(createElement)
-
-       handleReturn.createParagraph[0].innerText = BTC[index][0]
-       handleReturn.createParagraph[1].innerText = calculo 
-       
-      return 
-      } 
-          
-      let novoValor = document.querySelectorAll('.value-conversor')
-      novoValor.forEach((item, index)=>{
-        let novoCalculo = Number(BTCvalue * BTC[index][1].sell).toFixed(0)
-        console.log(item)
-        item.innerHTML = novoCalculo
-      })  
-    })
     
-    verificadorConversor = true
+    setTimeout(() => {
+      window.scroll({
+        top: document.body.scrollHeight,
+        behavior: "smooth",
+      });
+    }, 100);
+
+    console.log(document.body.scrollHeight)
   }
 
-  function scroll(){
-    setTimeout(()=>{
-      window.scrollTo({
-        top:document.body.scrollHeight,
-        behavior:"smooth"
-      })
-    }, 30)
+  function converterHandler(BTC) {
+    let BTCvalue = conversorInput.value;
+    if (BTCvalue === "" || BTCvalue === "0") {
+      alert("Preencha corretamente");
+      conversorInput.value = "";
+      return;
+    }
+
+    BTC.forEach((item, index) => {
+      let calculo = Number(BTCvalue * BTC[index][1].sell).toFixed(0);
+      if (verificadorConversor === false) {
+        let createElement = document.createElement("div");
+        let handleReturn = handleElements(
+          "title-conversor",
+          "value-conversor",
+          createElement
+        );
+
+        createElement.classList.add(
+          "moedaBtc-conversor",
+          "showOn",
+          "convertido"
+        );
+        painelConvesor.classList.add("growHeigth");
+
+        painelConvesor.appendChild(createElement);
+
+        handleReturn.createParagraph[0].innerText = BTC[index][0];
+        handleReturn.createParagraph[1].innerText = calculo;
+
+        return;
+      }
+
+      let novoValor = document.querySelectorAll(".value-conversor");
+      novoValor.forEach((item, index) => {
+        let novoCalculo = Number(BTCvalue * BTC[index][1].sell).toFixed(0);
+        console.log(item);
+        item.innerHTML = novoCalculo;
+      });
+    });
+
+    verificadorConversor = true;
   }
 
-  BtnConversor.addEventListener('click', scroll)
-  BtnConversor.addEventListener('click',  fetchBTCconversorMoeda)
+  BtnConversor.addEventListener("click", fetchBTCconversorMoeda);
 }
